d4902e130c440cb08382b6105e57e4cb0c97a31e
Bunye, Hannah Rae <101611020+hannahrae-bunye-lspu@users.noreply.github.com>
2022-10-10 03:40:49 +0000

    Update app.py

--- Activity 5/flask_test/app.py
+++ Activity 5/flask_test/app.py
@@ -15,16 +15,4 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
-@app.route('/act1')
-def act1():
-    return render_template('act1.html')
-
-@app.route('/act2')
-def act2():
-    return render_template('act2.html')
-
-@app.route('/act5')
-def act5():
-    return render_template('act5.html')
-
 app.run(host="localhost", debug=True)
